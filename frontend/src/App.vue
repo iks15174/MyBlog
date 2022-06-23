055b09ef7936e2a1a33e39d55e15b2f9dfc53fb3
ParkJiHo <iks15174@daum.net>
2022-06-23 23:34:54 +0900

    feat: add frontend skeleton code

---
+++ frontend/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <PatchMeta />
+  <NavBar
+    :title="'📝 iks15174'"
+    :sections="blogSections"
+  />
+  <Suspense>
+    <template #default>
+      <router-view />
+    </template>
+    <template #fallback>
+      <Loader />
+    </template>
+  </Suspense>
+  <Footer />
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue'
+import PatchMeta from '@/components/PatchMeta.vue'
+import Loader from '@/components/Loader.vue'
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'github-markdown-css/github-markdown.css'
+
+export default defineComponent({
+  components: {
+    PatchMeta,
+    Loader,
+    NavBar,
+    Footer
+  },
+  setup () {
+    const blogSections = inject('blogSections', {})
+    return { blogSections }
+  }
+})
+</script>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,116 @@
+<template>
+  <nav
+    class="navbar navbar-expand-md navbar-light mb-0"
+    :style="
+      `background-color: ${VUE_APP_NAVBAR_BG_CSS_COLOR}; color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`
+    "
+  >
+    <router-link
+      class="navbar-brand"
+      :to="'/'"
+      :style="`color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`"
+    >
+      {{ title }}
+    </router-link>
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#headerContent"
+      aria-controls="headerContent"
+      aria-expanded="false"
+      :style="`color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`"
+    >
+      <i class="bi bi-list"></i>
+    </button>
+
+    <div class="collapse navbar-collapse" id="headerContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link
+            class="nav-link"
+            :to="'/'"
+            :style="`color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`"
+          >
+            홈
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link
+            class="nav-link"
+            :to="'/'"
+            :style="`color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`"
+          >
+            태그
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link
+            class="nav-link"
+            :to="'/'"
+            :style="`color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`"
+          >
+            방명록
+          </router-link>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown-md">
+          <a
+            class="nav-link"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-expanded="false"
+            :style="`color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`"
+          >
+            <i class="bi bi-list"></i>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <router-link class="dropdown-item" :to="'/'">
+              내정보
+            </router-link>
+            <router-link class="dropdown-item" :to="'/'">
+              글작성
+            </router-link>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script language="ts">
+import { defineComponent, ref } from 'vue'
+
+const {
+  VUE_APP_NAVBAR_BG_CSS_COLOR = 'black',
+  VUE_APP_NAVBAR_TEXT_CSS_COLOR = 'white'
+} = process.env
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    sections: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  setup () {
+    const showDropdown = ref(false)
+    const collapseMenu = ref(true)
+
+    return {
+      showDropdown,
+      collapseMenu,
+      VUE_APP_NAVBAR_BG_CSS_COLOR,
+      VUE_APP_NAVBAR_TEXT_CSS_COLOR
+    }
+  }
+})
+</script>
