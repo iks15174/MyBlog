0e225f1a91d91c3f4bb8048d4c2ac68043aa6952
ParkJiHo <iks15174@daum.net>
2022-06-25 14:16:19 +0900

    feat/frontend: 댓글 입력, 댓글 아이템 컴포넌트 생성

---
+++ frontend/src/components/Comment.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="comment-container my-3">
+    <div class="comment-input">
+      <div class="input-group">
+        <textarea
+          class="form-control"
+          rows="3"
+          placeholder="댓글을 입력해주세요."
+        ></textarea>
+      </div>
+      <div class="button-group my-1 text-right">
+        <button class="btn btn-primary">등록</button>
+      </div>
+    </div>
+    <div class="commet-list">
+      <CommentItem></CommentItem>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CommentItem from '@/components/CommentItem.vue'
+const {
+  VUE_APP_MAIN_BG_CSS_COLOR = 'white',
+  VUE_APP_MAIN_TEXT_CSS_COLOR = 'black'
+} = process.env
+
+export default defineComponent({
+  components: {
+    CommentItem
+  },
+  setup () {
+    console.log('comment')
+  }
+})
+</script>
---
+++ frontend/src/components/CommentItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="row">
+        <div class="col-2">
+          <img
+            src="https://image.ibb.co/jw55Ex/def_face.jpg"
+            class="img img-rounded img-fluid"
+          />
+          <p class="text-secondary text-center">15 Minutes Ago</p>
+        </div>
+        <div class="col-10">
+          <p>
+            <a
+              class="float-left"
+              href="https://maniruzzaman-akash.blogspot.com/p/contact.html"
+              ><strong>Maniruzzaman Akash</strong></a
+            >
+          </p>
+          <div class="clearfix"></div>
+          <p>
+            Lorem Ipsum is simply dummy text of the pr make but also the leap
+            into electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem
+            Ipsum.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+const {
+  VUE_APP_MAIN_BG_CSS_COLOR = 'white',
+  VUE_APP_MAIN_TEXT_CSS_COLOR = 'black'
+} = process.env
+
+export default defineComponent({
+  setup () {
+    console.log('comment')
+  }
+})
+</script>
