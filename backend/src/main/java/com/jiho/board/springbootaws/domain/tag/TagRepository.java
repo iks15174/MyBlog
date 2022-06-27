7bcefb0f544039ae7cf546c328b22e76c8764b6b
ParkJiHo <iks15174@daum.net>
2022-06-27 16:07:04 +0900

    feat/backend: Tag entity & repository 추가

---
+++ backend/src/main/java/com/jiho/board/springbootaws/domain/tag/TagRepository.java
@@ -0,0 +1,7 @@
+package com.jiho.board.springbootaws.domain.tag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    
+}
