de836fba3f877d01d56b022b2273d0a51a858286
ParkJiHo <iks15174@daum.net>
2022-06-30 23:36:36 +0900

    feat/backend: Post 엔티티에 tags 속성 추가

--- backend/src/main/java/com/jiho/board/springbootaws/domain/posts/Posts.java
+++ backend/src/main/java/com/jiho/board/springbootaws/domain/posts/Posts.java
@@ -1,5 +1,9 @@
 package com.jiho.board.springbootaws.domain.posts;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -7,9 +11,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.jiho.board.springbootaws.domain.BaseTimeEntity;
 import com.jiho.board.springbootaws.domain.member.Member;
+import com.jiho.board.springbootaws.domain.postTag.PostTag;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,8 +43,15 @@ public class Posts extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private Member author;
 
+    @OneToMany(fetch = FetchType.LAZY, mappedBy="posts", cascade = CascadeType.ALL)
+    private List<PostTag> tags = new ArrayList<>();
+
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public void setTags(List<PostTag> ptags) {
+        this.tags = ptags;
     }
 }
