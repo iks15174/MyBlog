11aed5ae0b4b85591fd8d98ed4d560b579067e7a
ParkJiHo <iks15174@daum.net>
2022-07-01 17:11:15 +0900

    feat/backend: PostRepository에 findByIdWithTags 쿼리 추가

--- backend/src/main/java/com/jiho/board/springbootaws/domain/posts/PostsRepository.java
+++ backend/src/main/java/com/jiho/board/springbootaws/domain/posts/PostsRepository.java
@@ -9,11 +9,18 @@ import org.springframework.data.repository.query.Param;
 import com.jiho.board.springbootaws.domain.posts.search.SearchPostRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostsRepository extends JpaRepository<Posts, Long>, SearchPostRepository {
 
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
+
+    @Query("select distinct p" +
+    " from Posts p left outer join fetch PostTag pt on pt.posts = p " +
+    " left outer join fetch Tag t on pt.tag = t "+
+    " where p.id = :id")
+    Optional<Posts> findByIdWithTags(@Param("id") Long id);
 
     @Query("SELECT p FROM Posts p WHERE p.title LIKE %:title% OR p.content LIKE %:content%")
     Page<Posts> findAllSearch(@Param("title") String title, @Param("content") String content, Pageable pageable);
