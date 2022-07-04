536a5eb5fecfcb6b65028ea4c9ab4c84caa37050
ParkJiHo <iks15174@daum.net>
2022-07-04 23:31:23 +0900

    feat/backend : 특정 post 응답하는 PostResponseDto에 tag 정보 추가

--- backend/src/main/java/com/jiho/board/springbootaws/web/dto/posts/PostsResponseDto.java
+++ backend/src/main/java/com/jiho/board/springbootaws/web/dto/posts/PostsResponseDto.java
@@ -1,6 +1,11 @@
 package com.jiho.board.springbootaws.web.dto.posts;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jiho.board.springbootaws.domain.postTag.PostTag;
 import com.jiho.board.springbootaws.domain.posts.Posts;
+import com.jiho.board.springbootaws.web.dto.common.TagDto;
 
 import lombok.Getter;
 
@@ -10,11 +15,15 @@ public class PostsResponseDto {
     private String title;
     private String content;
     private String author;
+    private List<TagDto> tags = new ArrayList<TagDto>();
 
     public PostsResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
         this.author = entity.getAuthor().getName();
+        for(PostTag postTag:entity.getTags()){
+            this.tags.add((new TagDto(postTag.getTag())));
+        }
     }
 }
