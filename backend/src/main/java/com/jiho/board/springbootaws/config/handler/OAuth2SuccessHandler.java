61be57058836e792f07a266e19b71c2c4ceb34d5
ParkJiHo <iks15174@daum.net>
2022-07-25 23:47:38 +0900

    feat/backend : Oauth login할 때 refresh token을 유저 정보에 저장하도록 success handler 수정

--- backend/src/main/java/com/jiho/board/springbootaws/config/handler/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/jiho/board/springbootaws/config/handler/OAuth2SuccessHandler.java
@@ -5,8 +5,13 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jiho.board.springbootaws.domain.member.Member;
+import com.jiho.board.springbootaws.domain.member.MemberRepository;
+import com.jiho.board.springbootaws.exception.exceptions.CustomBasicException;
+import com.jiho.board.springbootaws.exception.exceptions.ErrorCode;
 import com.jiho.board.springbootaws.service.member.dto.AuthMemberDto;
 import com.jiho.board.springbootaws.util.JWTUtil;
 import com.jiho.board.springbootaws.web.dto.member.TokenDto;
@@ -22,9 +27,11 @@ import lombok.RequiredArgsConstructor;
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final JWTUtil jwtUtil;
+    private final MemberRepository memberRepository;
     private final ObjectMapper objectMapper;
 
     @Override
+    @Transactional
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
         AuthMemberDto authMemberDto = (AuthMemberDto) authentication.getPrincipal();
@@ -35,6 +42,8 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
             response.setCharacterEncoding("UTF-8");
             response.getWriter()
                     .write(objectMapper.writeValueAsString(tokenDto));
+            Member member = memberRepository.findByEmail(authMemberDto.getUsername()).orElseThrow(() -> new CustomBasicException(ErrorCode.UNEIXIST_USER));
+            member.updateRefreshToken(tokenDto.getRefreshToken());
         } catch (Exception e) {
             throw new IOException();
         }
