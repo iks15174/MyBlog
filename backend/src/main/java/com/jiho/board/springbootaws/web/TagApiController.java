e45bb9182a4410f746469446d2bebfdcfb932c82
ParkJiHo <iks15174@daum.net>
2022-07-27 12:55:33 +0900

    feat/backend : TagApi 응답을 ResponseEntity로 변경

--- backend/src/main/java/com/jiho/board/springbootaws/web/TagApiController.java
+++ backend/src/main/java/com/jiho/board/springbootaws/web/TagApiController.java
@@ -2,6 +2,8 @@ package com.jiho.board.springbootaws.web;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,20 +27,20 @@ public class TagApiController {
     private final TagService tagService;
 
     @GetMapping("/api/v1/tags")
-    public List<TagResponseDto> getTagList(@RequestParam(value = "name", required = false, defaultValue = "") String name) {
-        return tagService.getList(name);
+    public ResponseEntity<List<TagResponseDto>> getTagList(@RequestParam(value = "name", required = false, defaultValue = "") String name) {
+        return ResponseEntity.ok().body(tagService.getList(name));
     }
 
     @Secured(MemberRole.ROLES.USER)
     @PostMapping("/api/v1/tags")
-    public Long tagSave(@RequestBody TagSaveRequestDto requestDto) {
-        return tagService.save(requestDto);
+    public ResponseEntity<Long> tagSave(@RequestBody TagSaveRequestDto requestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(tagService.save(requestDto));
     }
 
     @Secured(MemberRole.ROLES.ADMIN)
     @PutMapping("/api/v1/tags/{id}")
-    public Long update(@PathVariable Long id, @RequestBody TagSaveRequestDto requestDto){
-        return tagService.update(id, requestDto);
+    public ResponseEntity<Long> update(@PathVariable Long id, @RequestBody TagSaveRequestDto requestDto){
+        return ResponseEntity.ok().body(tagService.update(id, requestDto));
     }
 
     
