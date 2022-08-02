4cb6c7bb4d33d83f580acf27a609b464edd85651
ParkJiHo <iks15174@daum.net>
2022-08-03 01:23:03 +0900

    feat/deploy : 한번에 배포 가능한 deploy.sh추가

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+source ./backend/update_backend.sh
+source ./frontend/update_frontend.sh
+
+echo "Move to backend directory"
+cd backend
+
+echo "Build backend file"
+./gradlew clean build -x test
+cd ..
+
+echo "Check project aleary deployed"
+FRONT_CONTAINER_NM=front
+ALEADY_DELPOYED=$(docker ps --filter "name=$FRONT_CONTAINER_NM" | wc -l)
+if [ ${ALEADY_DELPOYED} -gt 1 ];
+then
+    echo "Project aleardy deployed before"
+    update_backend_container
+    update_front_container
+    
+
+else
+    echo "Project not deployed"
+    docker compose up -d
+fi
