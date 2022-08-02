0baceec875a0f4709e681466da6e3b61869bcbf7
ParkJiHo <iks15174@daum.net>
2022-08-03 01:22:30 +0900

    feat/deploy : docker compose를 다시 실행하는 것이 아니라 프론트, 백엔드 각각 업데이트하는 쉘 스크립트 추가

---
+++ backend/update_backend.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+function update_backend_container() {
+    BACKEND1_SERVICE_NM=back1
+    BACKEND2_SERVICE_NM=back2
+    echo "Update first backend docker images and restart"
+
+    docker compose build $BACKEND1_SERVICE_NM # 첫번째 backend service를 다시 빌드한다.
+    docker compose restart $BACKEND1_SERVICE_NM # 첫번째 backend service를 다시 시작한다.
+
+    echo "Update second backend docker images and restart"
+
+    docker compose build $BACKEND2_SERVICE_NM # 두번째 backend service를 다시 빌드한다.
+    docker compose restart $BACKEND2_SERVICE_NM # 두번째 backend service를 다시 시작한다.
+}
---
+++ frontend/update_frontend.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+function update_front_container() {
+    FRONT_SERVICE_NM=front
+    echo "Update front container"
+
+    docker cp ./frontend/dist frontend:/usr/share/nginx/html # 새로운 빌드 파일로 교체
+    docker exec -it frontend nginx -s reload # nginx를 다시 reload한다.
+}
