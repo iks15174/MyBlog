00fe1800d1088b95f8da2822e1719e545fffa0c7
ParkJiHo <iks15174@daum.net>
2022-08-25 22:19:53 +0900

    chore: 사용하지 않는 import 제거

--- backend/src/main/java/com/jiho/board/springbootaws/web/MemberApiController.java
+++ backend/src/main/java/com/jiho/board/springbootaws/web/MemberApiController.java
@@ -1,16 +1,12 @@
 package com.jiho.board.springbootaws.web;
 
 import com.jiho.board.springbootaws.service.member.MemberService;
-import com.jiho.board.springbootaws.service.member.dto.AuthMemberDto;
 import com.jiho.board.springbootaws.web.dto.member.LoginRequestDto;
 import com.jiho.board.springbootaws.web.dto.member.MemberResponseDto;
 import com.jiho.board.springbootaws.web.dto.member.MemberSaveRequestDto;
 import com.jiho.board.springbootaws.web.dto.member.TokenDto;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
