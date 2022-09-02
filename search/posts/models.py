90092c77a5427da3c605b1ac61408c1bd99ea183
ParkJiHo <iks15174@daum.net>
2022-09-02 23:31:36 +0900

    feat: posts model 생성

--- search/posts/models.py
+++ search/posts/models.py
@@ -1,3 +1,20 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    post_id = models.BigIntegerField()
+    content = models.TextField(null=False)
+    contentType = models.CharField(max_length=100, default="text")
+
+class Words(models.Model):
+    word = models.CharField(max_length=200, null=False, unique=True)
+    
+
+class WordsPosts(models.Model):
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    word = models.ForeignKey(Words, on_delete=models.CASCADE)
+    appear_start = models.IntegerField(null=False)
+    appear_end = models.IntegerField(null=False)
+    
+    
