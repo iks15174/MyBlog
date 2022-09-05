9432b189ce4ec35b63a718e766ea8da26e877960
ParkJiHo <iks15174@daum.net>
2022-09-06 01:13:44 +0900

    feat: update 쉡스크립트의 버그 수정

--- backend/update_backend.sh
+++ backend/update_backend.sh
@@ -7,10 +7,10 @@ function update_backend_container() {
     echo "Update first backend docker images and restart"
 
     docker compose build $BACKEND1_SERVICE_NM # 첫번째 backend service를 다시 빌드한다.
-    docker compose up -d --no-deps $BACKEND1_SERVICE_NM # 첫번째 backend service를 다시 시작한다.
+    docker compose up -d $BACKEND1_SERVICE_NM # 첫번째 backend service를 다시 시작한다.
 
     echo "Update second backend docker images and restart"
 
     docker compose build $BACKEND2_SERVICE_NM # 두번째 backend service를 다시 빌드한다.
-    docker compose up -d --no-deps  $BACKEND2_SERVICE_NM # 두번째 backend service를 다시 시작한다.
+    docker compose up -d $BACKEND2_SERVICE_NM # 두번째 backend service를 다시 시작한다.
 }
--- frontend/update_frontend.sh
+++ frontend/update_frontend.sh
@@ -5,6 +5,6 @@ function update_front_container() {
     FRONT_SERVICE_NM=front
     echo "Update front container"
 
-    docker cp ./frontend/dist frontend:/usr/share/nginx/html # 새로운 빌드 파일로 교체
-    docker exec -it frontend nginx -s reload # nginx를 다시 reload한다.
+    docker cp ./frontend/dist $FRONT_SERVICE_NM:/usr/share/nginx/html # 새로운 빌드 파일로 교체
+    docker exec -it $FRONT_SERVICE_NM nginx -s reload # nginx를 다시 reload한다.
 }
