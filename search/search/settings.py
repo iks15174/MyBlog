6ad4953fc38c570acd381b4a65490e5092691332
ParkJiHo <iks15174@daum.net>
2022-09-05 16:09:07 +0900

    feat: mysql 연결을 위한 설정 추가

--- search/search/settings.py
+++ search/search/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'search.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'search',              
+        'USER': 'root',                      
+        'PASSWORD': 'password',              
+        'HOST': 'localhost',                 
+        'PORT': '3306',                      
     }
 }
 
