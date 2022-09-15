a44bdff3fca1e8f944846d72760bec9cf6014f31
ParkJiHo <iks15174@daum.net>
2022-09-15 18:42:06 +0900

    feat: aop를 위한 dependency 및 annotation 추가

--- backend/src/main/java/com/jiho/board/springbootaws/SpringBootAwsApplication.java
+++ backend/src/main/java/com/jiho/board/springbootaws/SpringBootAwsApplication.java
@@ -3,9 +3,11 @@ package com.jiho.board.springbootaws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class SpringBootAwsApplication {
 
 	public static void main(String[] args) {
